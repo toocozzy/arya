1464ad16348be3589fe23f04d34546b8ea3f1b7e
Oskar Daszkowski <oskar.daszkowski@gmail.com>
2022-03-27 17:53:45 +0200

    remodel navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Logo from "./components/Logo/Logo";
 
 function App() {
   return (
     <div className="App">
       <header>
+        <Logo />
         <Navbar />
       </header>
     </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,25 @@
 import "../../styles/Navbar.css";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
-      <div className="navbar__logo-container">
-        <span className="navbar__logo">arya</span>
-      </div>
       <ul className="navbar__links-container">
         <NavLink to="/" className="navbar__link">
-          <i className="ri-home-line"></i> Home
+          <i className="ri-home-line navbar__link-icon"></i>
+          <span className="navbar__link-text">Home</span>
         </NavLink>
         <NavLink to="/" className="navbar__link">
-          <i className="ri-article-line"></i> News
+          <i className="ri-article-line navbar__link-icon"></i>
+          <span className="navbar__link-text">News</span>
         </NavLink>
         <NavLink to="/" className="navbar__link">
-          <i className="ri-line-chart-fill"></i> Cryptocurrencies
+          <i className="ri-line-chart-fill navbar__link-icon"></i>
+          <span className="navbar__link-text">Cryptocurrencies</span>
         </NavLink>
         <NavLink to="/" className="navbar__link">
-          <i className="ri-exchange-line"></i> Exchanges
+          <i className="ri-exchange-line navbar__link-icon"></i>
+          <span className="navbar__link-text">Exchanges</span>
         </NavLink>
       </ul>
     </nav>
